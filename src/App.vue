a2e118f0226375a17cbb6084b15b1e3b52c7e1b7
unknown <feperillal@correo.udistrital.edu.co>
2022-06-05 22:36:44 -0500

    clase 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
  <div class="">
-   <h1>{{imputText}} ! </h1>
+   <h1><center>{{mensaje}} ! </center></h1>
    <h2 :class="clasepruebas" v-text="texto"></h2>
    <p v-html="textohtml"></p>
    <input type="text" v-model="imputText">
-   <img :src="img" alt="">
+   <br>
+   <center><img :src="img" alt=""></center>
+   <br>
+   <br>
+   <button @click="incrementarcontador()">
+     Boton {{contador}}
+   </button>
+   <input type="text" @keyup="pressbutton()" v-model="inputEvent ">
+   <select @change="cambiarcolor()" v-model="colorContenedor">
+     <option value="contenedor rojo">rojo</option>
+     <option value="contenedor verde">verde</option>
+     <option value="contenedor purpura">purple</option>
+     <option value="contenedor gris">gray</option>
+   </select>
+   <br>
+   <div :class="contenedor"></div>
+   <br>
+   <form @submit="enviarFormulario">
+     <input type="text" v-model="inputform">
+     <!-- <textarea cols="30" rows="10" v-model= "inputform"></textarea> -->
+     <input type="submit" value="Send">
+   </form>
+   <!-- <input type="text" @input= "cambiarParrafo" v-model= "inputform">-->
+   <br>
+   <p>{{parrafo}}</p>
  </div>
 </template>
 <script>
@@ -17,7 +41,30 @@ export default {
       textohtml: ' <a href="https://wax-metal-014.notion.site/Curso-VUE-JS-21c6d515f5b04698b04d5018b9524d2b"> Hola mundo pero en html </a> ',
       imputText: 'Escribe un texto',
       img: 'https://picsum.photos/200/300?grayscale',
-      clasepruebas: 'texto-azul'
+      clasepruebas: 'texto-azul',
+      contador: 0,
+      inputEvent: '',
+      contenedor: 'contenedor',
+      colorContenedor: 'contenedor-verde',
+      inputform: '',
+      parrafo: ''
+    }
+  },
+  methods: {
+    incrementarcontador () {
+      this.contador++
+    },
+    pressbutton () {
+      alert(this.inputEvent)
+    },
+    cambiarcolor () {
+      this.contenedor = this.colorContenedor
+    },
+    enviarFormulario () {
+      this.parrafo = this.inputform
+    },
+    cambiarParrafo () {
+      this.parrafo = this.inputform
     }
   }
 }
@@ -31,4 +78,23 @@ export default {
   color: red;
   background-color: pink;
 }
+.contenedor{
+  width: 100px;
+  height: 100px;
+  background-color: white;
+}
+.verde
+{
+  background-color: green;
+}
+.rojo{
+  background-color: red;
+}
+.purpura{
+  background-color: purple;
+}
+.gris{
+  background-color: gray;
+}
+
 </style>
